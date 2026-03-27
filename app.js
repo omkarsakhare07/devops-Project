5d46405b08695a863ee5463cfd42200747770121
omkarsakhare07 <omkarsakhare87@gmail.com>
2026-03-28 00:42:17 +0530

    test CI/CD

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Omkar's DevOps Project 🚀");
+  res.send("Hello from Omkar to Raju 🚀");
 });
 
 app.listen(3000, () => {
