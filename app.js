4503f52d497b2b50e99cb78abf0f755ca6318730
omkarsakhare07 <omkarsakhare87@gmail.com>
2026-07-11 12:08:39 +0530

    updated app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello,This is my First DevOps project");
+  res.send("Congratssss");
 });
 
 app.listen(3000, () => {
